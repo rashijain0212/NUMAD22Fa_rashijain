dba94cced1d04e362e3c69f0ab5c741487270d98
rashijain <rashijain2637@gmail.com>
2022-09-25 19:07:47 -0400

    Assignment : Clicky Clicky

---
+++ app/src/main/java/edu/northeastern/numad22fa_rashijain/AnActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad22fa_rashijain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_an);
+    }
+}
--- app/src/main/java/edu/northeastern/numad22fa_rashijain/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_rashijain/MainActivity.java
@@ -6,10 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     private Button About_btn;
+    private Button A;
+    private Button B;
+    private Button C;
+    private Button D;
+    private Button E;
+    private Button F;
+    private TextView tv;
 
 
     @Override
@@ -17,6 +26,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         About_btn = findViewById(R.id.AboutMe);
+        A = findViewById(R.id.A);
+        B = findViewById(R.id.B);
+        C = findViewById(R.id.C);
+        D = findViewById(R.id.D);
+        E = findViewById(R.id.E);
+        F = findViewById(R.id.F);
+        tv = findViewById(R.id.textView);
 
         About_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,8 +41,78 @@ public class MainActivity extends AppCompatActivity {
             }
 
             private void About_Me_Fn() {
-                Toast.makeText(getApplicationContext(),"Name :- Rashi Jain and Email :- jain.ras@northeastern.edu",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Name :- Rashi Jain and Email :- jain.ras@northeastern.edu", Toast.LENGTH_LONG).show();
             }
         });
+        A.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                A_pressed();
+            }
+
+            private void A_pressed() {
+                tv.setText("Pressed: A");
+            }
+        });
+        B.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                B_pressed();
+            }
+
+            private void B_pressed() {
+                tv.setText("Pressed: B");
+
+            }
+        });
+        C.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                C_pressed();
+            }
+
+            private void C_pressed() {
+                tv.setText("Pressed: C");
+
+            }
+        });
+        D.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                D_pressed();
+            }
+
+            private void D_pressed() {
+                tv.setText("Pressed: D");
+
+            }
+        });
+        E.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                E_pressed();
+            }
+
+            private void E_pressed() {
+                tv.setText("Pressed: E");
+            }
+        });
+        F.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                F_pressed();
+            }
+
+            private void F_pressed() {
+                tv.setText("Pressed: F");
+            }
+        });
+
+
+    }
+
+    public void goToAnActivity(View view) {
+        Intent intent = new Intent(this, AnActivity.class);
+        startActivity(intent);
     }
 }
