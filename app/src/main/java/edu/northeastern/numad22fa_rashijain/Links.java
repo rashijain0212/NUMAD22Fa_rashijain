b234a471856bd2c3ca98ca4e02fee01efd26c52a
rashijain <rashijain2637@gmail.com>
2022-10-02 17:53:32 -0400

    Assignment : Link Collector

---
+++ app/src/main/java/edu/northeastern/numad22fa_rashijain/Links.java
@@ -0,0 +1,23 @@
+package edu.northeastern.numad22fa_rashijain;
+
+public class Links {
+    private final String name;
+
+    private final String url;
+
+    public Links(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getURL() {
+        return url;
+    }
+}
+
+
+
