1f1356549a3e3be098e0a21b1bd816616d6e5158
rashijain <rashijain2637@gmail.com>
2022-10-09 12:44:07 -0400

    Assignment : Search Prime

--- app/src/main/java/edu/northeastern/numad22fa_rashijain/Prime.java
+++ app/src/main/java/edu/northeastern/numad22fa_rashijain/Prime.java
@@ -18,45 +18,45 @@ public class Prime extends AppCompatActivity {
     Button Terminate_Search;
     boolean flag;
     Handler handler;
-
     Handler handler1;
     int is_prime;
     int num;
-
-
     String string1;
-
-
     String string2;
-
-
-    Thread__ myThread;
-
+    Prime.myThread myThread;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prime);
-        this.handler = new Handler();
-        this.handler1 = new Handler();
-        this.myThread = new Thread__();
-        this.Find_Primes = findViewById(R.id.FindPrimes);
-        this.Terminate_Search = findViewById(R.id.TerminateSearch);
-        this.Current_Number = findViewById(R.id.Current_Number);
-        this.Latest_PrimeNumber_Found = findViewById(R.id.LatestPrimeNumberFound);
+        handler = new Handler();
+        handler1 = new Handler();
+        myThread = new myThread();
+        Find_Primes = findViewById(R.id.FindPrimes);
+        Terminate_Search = findViewById(R.id.TerminateSearch);
+        Current_Number = findViewById(R.id.Current_Number);
+        Latest_PrimeNumber_Found = findViewById(R.id.LatestPrimeNumberFound);
 
-        this.Find_Primes.setOnClickListener(view -> {
-            Prime.this.flag = true;
-            Thread__ runnableThread = new Thread__();
+        Find_Primes.setOnClickListener(view -> {
+            flag = true;
+            Prime.myThread runnableThread = new myThread();
             new Thread(runnableThread).start();
         });
-        this.Terminate_Search.setOnClickListener(view -> Prime.this.flag = false);
+        Terminate_Search.setOnClickListener(view -> flag = false);
     }
 
 
     public void onBackPressed() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Do you want to terminate activity?");
-        builder.setNegativeButton("Ok", (dialogInterface, i) -> Prime.this.goToActivity());
+        if (flag) {
+            builder.setMessage("Do you want to terminate activity while search is going?");
+
+        } else {
+            builder.setMessage("Do you want to terminate activity?");
+        }
+        builder.setNegativeButton("Ok", (dialogInterface, i) -> {
+            flag = false;
+            Prime.this.goToActivity();
+        });
         builder.setPositiveButton("Cancel", (dialogInterface, i) -> {
         });
         builder.create().show();
@@ -66,44 +66,42 @@ public class Prime extends AppCompatActivity {
         startActivity(new Intent(this, MainActivity.class));
     }
 
-    class Thread__ implements Runnable {
-        Thread__() {
+    class myThread implements Runnable {
+        myThread() {
         }
 
         public void run() {
-            Prime.this.num = 2;
-            while (Prime.this.flag) {
-                Prime.this.is_prime = 0;
-                for (int i = 2; i <= Prime.this.num; i++) {
+            num = 2;
+            while (flag) {
+                is_prime = 0;
+                for (int i = 2; i <= num; i++) {
                     try {
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    if (Prime.this.num % i == 0) {
-                        Prime.this.is_prime = 1;
+                    if (num % i == 0) {
+                        is_prime = 1;
                     }
-                    if (Prime.this.is_prime == 0 && i == Prime.this.num - 1) {
-                        Prime.this.handler.post(new Thread__() {
+                    if (is_prime == 0 && i == num - 1) {
+                        handler.post(new myThread() {
 
                             public void run() {
                                 Prime thread_Activity = Prime.this;
-                                thread_Activity.string2 = "Latest Prime Number found is:- " + Prime.this.num;
-                                Prime.this.Latest_PrimeNumber_Found.setText(Prime.this.string2);
+                                thread_Activity.string2 = "Latest Prime Number found is:- " + num;
+                                Latest_PrimeNumber_Found.setText(string2);
                             }
                         });
                     }
-                    Prime.this.handler1.post(new Thread__() {
+                    handler1.post(new myThread() {
                         public void run() {
-                            Prime.this.string1 = "Current  Number is:- " + Prime.this.num;
-                            Prime.this.Current_Number.setText(Prime.this.string1);
+                            string1 = "Current  Number is:- " + num;
+                            Current_Number.setText(string1);
                         }
                     });
                 }
-                Prime.this.num++;
+                num++;
             }
         }
     }
-
-
 }
