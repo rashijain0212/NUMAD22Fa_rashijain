71cb72b78ff5d3c270703d0a4efe1e2f2312af2c
rashijain <rashijain2637@gmail.com>
2022-10-12 16:18:53 -0400

    Assignment : Location

---
+++ app/src/main/java/edu/northeastern/numad22fa_rashijain/MyLocation.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad22fa_rashijain;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_location);
+    }
+}
